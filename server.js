6483cbea041a92f475e850d51b5561a875b6a878
CircularitySpace <circularity.ke@gmail.com>
2023-11-24 07:24:48 -0600

    Done the job and user name

--- server.js
+++ server.js
@@ -1,86 +1,72 @@
-// server.js
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const socketio = require('socket.io');
 const http = require('http');
 
-const firebaseAdmin = require('./firebaseAdmin');
-const taskRoutes = require('./routes/taskRoutes'); // Make sure this path is correct
+// Import routes
+const taskRoutes = require('./routes/taskRoutes');
 const userRoutes = require('./routes/userRoutes');
 const jobRoutes = require('./routes/jobRoutes');
 const reportRoutes = require('./routes/reportRoutes');
 const mapRoutes = require('./routes/mapRoutes');
 const chatRoutes = require('./routes/chatRoutes');
 const messageRoutes = require('./routes/messageRoutes');
-const clientRoutes = require('./routes/clientRoutes'); 
-const pendingTasksRoutes = require('./routes/pendingTaskRoutes'); // Update path as needed
+const clientRoutes = require('./routes/clientRoutes');
+const pendingTasksRoutes = require('./routes/pendingTaskRoutes');
 
-
+// Import middleware
 const authenticate = require('./middleware/authenticate');
 
 require('dotenv').config();
-console.log(process.env.PORT);
 
-const port = process.env.PORT || 5000;
 const app = express();
-const server = http.createServer(app); // Step 3: Create an HTTP server
-const io = socketio(server, {
-  cors: {
-    origin: "https://jutdo-7b90f.web.app/", // Replace with your front-end URL
-    methods: ["GET", "POST"]
-  }
-});
+const server = http.createServer(app);
 
-app.use(cors());
-app.use(express.json()); // For parsing application/json
+// CORS configuration
+const whitelist = ['http://localhost:3000', 'https://jutdo-7b90f.web.app'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+// Use CORS options here
+app.use(cors(corsOptions));
+
+// JSON parsing middleware
+app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
-  console.log('Cloudinary API Key:', process.env.CLOUDINARY_API_KEY);
-
 });
 
-// Step 5: Setup socket.io connection
-io.on('connection', (socket) => {
-  console.log('a user connected');
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-
-  // Handle chat message event
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg); // This will emit the event to all connected sockets
-  });
-});
-
-
-// Use routes for users
+// Routes
 app.use('/api/users', userRoutes);
-// Use routes for tasks
-app.use('/api/tasks', taskRoutes); 
-
+app.use('/api/tasks', taskRoutes);
 app.use('/api/jobs', jobRoutes);
-
 app.use('/api/reports', reportRoutes);
-
-app.use('/uploads', express.static('uploads'));
-
 app.use('/api/chats', chatRoutes);
-
 app.use('/api/messages', messageRoutes);
-
 app.use('/api/clients', clientRoutes);
-
-app.use('/api', pendingTasksRoutes);
-
+app.use('/api/pendingtasks', pendingTasksRoutes);
 app.use(mapRoutes);
 
+// Static directory for uploads
+app.use('/uploads', express.static('uploads'));
+
+// Handle preflight requests for all routes
+app.options('*', cors(corsOptions));
+
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+  console.log(`Server running on port ${port}`);
 });
